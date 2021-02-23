e35056c5ae25ddb2d4d3937304f7118e50e7b827
Ben Flynn <flynn0087@gmail.com>
2021-02-23 13:27:10 -0600

    removed image cache from service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,18 +3,14 @@ const DATA_CACHE_NAME = "data-cache-v1";
 const FILES_TO_CACHE = [
   "/",
   "/index.html",
+  "/index.js",
   "/icons/icon-192x192.png",
   "/icons/icon-512x512.png",
   "/style.css"
 ];
 
 // install
-self.addEventListener("install", function (evt) {
-  // pre cache image data
-  evt.waitUntil(
-    caches.open(DATA_CACHE_NAME).then((cache) => cache.add("/api/transaction"))
-  );
-    
+self.addEventListener("install", function (evt) {  
   // pre cache all static assets
   evt.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
