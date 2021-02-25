50e37ef9b6020286b02dee485b4f875034c1b658
Ben Flynn <flynn0087@gmail.com>
2021-02-24 23:40:02 -0600

    typo correction

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,7 +1,7 @@
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
 
-const IconSizes = ["192", "512"];
+const iconSizes = ["192", "512"];
 const iconFiles = iconSizes.map(
   (size) => `/icons/icon-${size}x${size}.png`
 );
