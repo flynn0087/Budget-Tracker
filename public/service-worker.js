68a7021f0f57978484918708dbc796875957ebe4
Ben Flynn <flynn0087@gmail.com>
2021-02-24 21:29:27 -0600

    further files added to cache

--- public/service-worker.js
+++ public/service-worker.js
@@ -5,7 +5,10 @@ const FILES_TO_CACHE = [
   "/index.html",
   "/index.js",
   "/styles.css",
-  "/manifest.webmanifest"
+  "/indexedDB.js",
+  "/manifest.webmanifest",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png"
 ];
 
 // install
