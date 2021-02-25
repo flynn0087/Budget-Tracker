7abce84404fd0d11e7bc16aead39d86dba0c3a5d
Ben Flynn <flynn0087@gmail.com>
2021-02-25 11:48:41 -0600

    further troubleshooting of indexedDB.js

--- public/indexedDB.js
+++ public/indexedDB.js
@@ -1,35 +1,41 @@
-let db = () => {
-    const request = window.indexedDB.open("budget", 1);
+let db;
+const request = window.indexedDB.open("budget", 1);
 
 //creates storage for the objects that are awating being "online"
-    request.onupgradeneeded = function(evt) {
-        const db = evt.target.result;
-        const budgetStore = db.createObjectStore("budget", { keyPath: "id", autoIncrement: true });
-        budgetStore.createIndex("nameIndex", "name");
-        budgetStore.createIndex("valueIndex", "value");
-        budgetStore.createIndex("dateIndex", "date");
-    };
-};    
-
-//creates a transaction on the budget DB, determines if online
-let saveRecord = (record) => {
-    const request = window.indexedDB.open("budget", 1);
-    request.onerror = function(evt) {
-        console.log("There was an error");
-    };
-    request.onsucess = () => {
-        const db = request.result;
-        const transaction = db.transaction(["budget"], "readwrite");
-        const budgetStore = transaction.objectStore("budget");
-        budgetStore.add({ name: record.name, value: record.value, date: record.date});
-    };
+request.onupgradeneeded = function(evt) {
+    const db = evt.target.result;
+    const budgetStore = db.createObjectStore("budget", { 
+        keyPath: "id", 
+        autoIncrement: true 
+    });
+    budgetStore.createIndex("amountIndex", "amount");
 };
 
-//this section compares the online database to the budget and merges them
+//checks if online before reading the database
+request.onsuccess = function(evt) {
+    db = evt.target.result;
+    if (navigator.onLine) {
+        checkDatabase();
+    }
+};
+
+//gives error if not online
+request.onerror = function(evt) {
+    console.log("There was an error");
+};
+
+//creates a transaction on the pending DB, store the object
+function saveRecord(record) {
+    const transaction = db.transaction(["budget"], "readwrite");
+    const budgetStore = transaction.objectStore("budget");
+    budgetStore.add(record);
+};
+
+//this section compares the online database to the pending and merges them
 function checkDatabase() {
     const transaction = db.transaction(["budget"], "readwrite");
-    const store = transaction.objectStore("budget");
-    const getAll = store.getAll();
+    const budgetStore = transaction.objectStore("budget");
+    const getAll = budgetStore.getAll();
 
     getAll.onsucess = function() {
         if (getAll.result.length > 0) {
@@ -44,8 +50,8 @@ function checkDatabase() {
             .then(response => response.json())
             .then(() => {
                 const transaction = db.transaction(["budget"], "readwrite");
-                const store = transaction.objectStore("budget");
-                store.clear();
+                const budgetStore = transaction.objectStore("budget");
+                budgetStore.clear();
             });
         }
     };
